0d9966a496f77889359f65582b4455c423928d6e
Cleon Souza <154812561+CleonDeSouza@users.noreply.github.com>
2025-04-28 08:29:21 +0100

    Update big.py

--- big.py
+++ big.py
@@ -16,11 +16,11 @@ For Data Science
 
 For Cybersecurity
 
-| Project Idea                     | What It Shows                                     | Extra Tips                                                 |
-|----------------------------------|---------------------------------------------------|------------------------------------------------------------|
-| 1. Crime Prediction Dashboard    | Data collection, ML model training, Visualization | Use real-world crime datasets (ex: Kaggle datasets)        |
-| 2. Stock Price Predictor         | Time series analysis, Regression models           | Maybe use real API data                                    |
-| 3. Smart Resume Analyzer         | Natural Language Processing (NLP), Automation     | Upload resumes → ML rates them for skills/keywords         |
-| 4. Disease Spread Simulation     | Data modeling, Prediction                         | Simulate how a virus/disease spreads using math modeling   |
-| 5. Real Estate Price Estimator   | Predict house prices with Machine Learning        | Practice feature engineering + model training              |
-| 6. AI Chatbot                    | NLP, AI/ML deployment                             | Can use Huggingface Transformers, fine-tune basic models   |
+| Project Idea                     | What It Shows                                     | Extra Tips                                                         |
+|----------------------------------|---------------------------------------------------|--------------------------------------------------------------------|
+| 1. Keylogger (Educational Only)  | Understand system security                        | Python scripting	For personal learning only!                       |
+| 2. Basic Vulnerability Scanner   | Networking knowledge, scanning tools              | Use Python libraries like socket, nmap, scapy                      |
+| 3. Secure File Storage App       | Encryption, Authentication                        | Learn cryptography with cryptography or pycryptodome               |
+| 4. Penetration Test Lab          | Hands-on pentesting skills                        | Setup safe environments: DVWA, OWASP Juice Shop, Metasploitable2   |
+| 5. Password Cracking Simulator   | Brute force attacks, hash understanding           | Write basic password crackers using dictionaries or permutations   |
+| 6. Log Analysis Tool             | Log parsing, threat detection                     | Analyze Apache/Nginx/system logs for anomalies                     |
