7a8a4c705ebfee44641d0a31810c69463af8e5fe
Cleon Souza <154812561+CleonDeSouza@users.noreply.github.com>
2025-04-28 08:29:45 +0100

    Update big.py

--- big.py
+++ big.py
@@ -18,7 +18,7 @@ For Cybersecurity
 
 | Project Idea                     | What It Shows                                     | Extra Tips                                                         |
 |----------------------------------|---------------------------------------------------|--------------------------------------------------------------------|
-| 1. Keylogger (Educational Only)  | Understand system security                        | Python scripting	For personal learning only!                       |
+| 1. Keylogger (Educational Only)  | Understand system security                        | Python scripting, For personal learning only                       |
 | 2. Basic Vulnerability Scanner   | Networking knowledge, scanning tools              | Use Python libraries like socket, nmap, scapy                      |
 | 3. Secure File Storage App       | Encryption, Authentication                        | Learn cryptography with cryptography or pycryptodome               |
 | 4. Penetration Test Lab          | Hands-on pentesting skills                        | Setup safe environments: DVWA, OWASP Juice Shop, Metasploitable2   |
