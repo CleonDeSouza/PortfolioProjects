dddd6c325e2643069aa15250eb9b2bc8b5f49706
Cleon Souza <154812561+CleonDeSouza@users.noreply.github.com>
2025-04-28 08:22:54 +0100

    Update big.py

--- big.py
+++ big.py
@@ -3,13 +3,18 @@ Big Projects For Portfolio (Ages 16-22)
 
 For Data Science
 
-Project Idea													What It Shows											Extra Tips
-1. Crime Prediction Dashboard	Data collection,                ML model training,                 						Visualization	Use real-world crime datasets (ex: Kaggle datasets)
-2. Stock Price Predictor Time series analysis,         	        Regression models	Python (scikit-learn), 				Maybe use real API data
-3. Smart Resume Analyzer 										Natural Language Processing (NLP), 						Automation	Upload resumes → ML rates them for skills/keywords
-4. Disease Spread Simulation									Data modeling, 											Prediction	Simulate how a virus/disease spreads using math modeling
-5. Real Estate Price Estimator									Predict house prices with Machine Learning,				Practice feature engineering + model training
-6. AI Chatbot													NLP, AI/ML deployment,	          						Can use Huggingface Transformers, fine-tune basic models
+| Project Idea                     | What It Shows                                     | Extra Tips                                                 |
+|----------------------------------|---------------------------------------------------|------------------------------------------------------------|
+| 1. Crime Prediction Dashboard    | Data collection, ML model training, Visualization | Use real-world crime datasets (ex: Kaggle datasets)        |
+| 2. Stock Price Predictor         | Time series analysis, Regression models           | Maybe use real API data                                    |
+| 3. Smart Resume Analyzer         | Natural Language Processing (NLP), Automation     | Upload resumes → ML rates them for skills/keywords         |
+| 4. Disease Spread Simulation     | Data modeling, Prediction                         | Simulate how a virus/disease spreads using math modeling   |
+| 5. Real Estate Price Estimator   | Predict house prices with Machine Learning        | Practice feature engineering + model training              |
+| 6. AI Chatbot                    | NLP, AI/ML deployment                             | Can use Huggingface Transformers, fine-tune basic models   |
+
+
+
+For Cybersecurity
 
 | Project Idea                     | What It Shows                                     | Extra Tips                                                 |
 |----------------------------------|---------------------------------------------------|------------------------------------------------------------|
